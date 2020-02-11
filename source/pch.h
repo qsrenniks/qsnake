8315d8ecd26e9138c11fa3688ab51ec01e668bd4
qsrenniks <q.skinner@digipen.edu>
2020-02-11 10:48:24 -0800

    added libraries to the projectg

---
+++ source/pch.h
@@ -0,0 +1,31 @@
+/*!
+ * \file pch.h
+ * \author Quinton Skinner
+ * \par Email: qsrenniks\@gmail.com
+ * \par 2020/02/11
+ */
+
+#ifndef PCH_H
+#define PCH_H
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_stdlib.h"
+
+#define GLM_FORCE_INLINE
+#define GLM_FORCE_PURE
+#include "glm/glm.hpp"
+#include "glm/matrix.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/constants.hpp"
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/rotate_vector.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtx/matrix_decompose.hpp"
+#include "glm/gtx/compatibility.hpp"
+
+#endif
